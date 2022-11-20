eaa280d77b31c02c1ff326b8ad8b4950f64a2457
L R <ljnetwerk@posteo.nl>
2022-11-20 20:06:45 +0100

    only send stand on range

--- cmd/stand.go
+++ cmd/stand.go
@@ -5,10 +5,15 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"time"
+
 	voetbalpoulestelegram "github.com/Carbonique/voetbalpoules/pkg/telegram"
 	voetbalpoules "github.com/Carbonique/voetbalpoules/pkg/voetbalpoules-client"
 	"github.com/spf13/cobra"
 )
+
+var timeStand int
 
 // standCmd represents the stand command
 var standCmd = &cobra.Command{
@@ -16,8 +21,25 @@ var standCmd = &cobra.Command{
 	Short: "sends the stand for a pool",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+
 		client := voetbalpoules.NewClient(BASE_URL)
 		bot := voetbalpoulestelegram.NewBot(TOKEN, CHAT)
+
+		// If flag is changed the user wants to only send stand if a match
+		// has started in the timerange
+		if cmd.Flags().Lookup("time").Changed {
+			t1 := time.Now()
+			t2 := t1.Add(time.Minute * time.Duration(timeStand))
+
+			w, _ := client.GetWedstrijden(t1, t2, POOL_ID, COMPETITIE)
+			fmt.Printf("w: %v\n", w)
+
+			if len(w) == 0 {
+				return
+			}
+
+		}
+
 		deelnemers := client.GetStand(POOL_ID, COMPETITIE)
 		bot.StuurStand(deelnemers)
 	},
@@ -25,5 +47,6 @@ var standCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(standCmd)
+	standCmd.PersistentFlags().IntVar(&timeStand, "time", 0, "Time in minutes from now to look for matches. Will only print stand if a match started within this timeframe")
 
 }
